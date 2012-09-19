588200446aa0468dca728832e72e0af9c2d70cca
Vasileios Topouz <vtopouz@cyclopsweb.gr>
2012-09-19 13:28:24 +0300

    getting there

--- app/controllers/user_controller.rb
+++ app/controllers/user_controller.rb
@@ -2,10 +2,13 @@ class UserController < ApplicationController
   
   def index
      @client = Geocoder.search(request.ip)
+     @client_city = Geocoder.search(request.location.city)
      respond_to do |format|
        format.html # index.html.erb
        format.json { render json: @client }
      end
     end
+ 
+ 
   end
   
