95c09b0f3152c6bc6224fe1cd746f99a7f9dbb5b
Vasileios Topouz <vtopouz@cyclopsweb.gr>
2012-09-21 23:52:59 +0300

    added more requests

--- app/controllers/user_controller.rb
+++ app/controllers/user_controller.rb
@@ -3,6 +3,9 @@ class UserController < ApplicationController
   def index
      @client = Geocoder.search(request.ip)
      @city = request.location.city
+     @ip = request.ip
+     @address = Geocoder.search("@ip")
+     
      respond_to do |format|
        format.html # index.html.erb
        format.json { render json: @client}
